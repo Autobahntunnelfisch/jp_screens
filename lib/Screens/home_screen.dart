292f81764c4be89c2bcb93ab5325acf0504f18ff
Autobahntunnelfisch <cankinik@web.de>
2025-01-24 12:23:58 +0100

    add background and first Text widget

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -10,6 +10,32 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/images/bg_mainscreen.png"),
+            fit: BoxFit.fill),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Positioned(
+              width: 350,
+              left: 20,
+              top: 70,
+              child: Text(
+                textAlign: TextAlign.left,
+                "Choose Your Favorite Snack",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w900),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
