e8870b0f21b663d743a96db01125431eff788ee6
Autobahntunnelfisch <cankinik@web.de>
2025-01-26 18:51:10 +0100

    create PopUpMenuButton for TabBar

---
+++ lib/widgets/popup_menu_button_for_tab_bar.dart
@@ -0,0 +1,37 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class PopupMenuButtonForTabBar extends StatelessWidget {
+  const PopupMenuButtonForTabBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Tab(
+      child: Container(
+        height: 40,
+        width: 120,
+        decoration: BoxDecoration(
+            color: Color.fromRGBO(255, 255, 255, 0.2),
+            borderRadius: BorderRadius.circular(50),
+            border: Border.all(
+                color: const Color.fromRGBO(255, 255, 255, 0.5), width: 1)),
+        child: PopupMenuButton(
+          icon: SvgPicture.asset("assets/svg/Lunch.svg"),
+          color: Color.fromRGBO(255, 255, 255, 0.2),
+          borderRadius: BorderRadius.circular(50),
+          shape:
+              Border.all(color: Color.fromRGBO(255, 255, 255, 0.5), width: 0.3),
+          itemBuilder: (context) => [
+            PopupMenuItem(child: Text("Salty")),
+            PopupMenuItem(child: Text("Sweet")),
+            PopupMenuItem(child: Text("Spicy")),
+            PopupMenuItem(child: Text("Sour")),
+          ],
+        ),
+      ),
+    );
+  }
+}
