07e51ed3015c2185d91a1b486269866aa3b58f0c
Autobahntunnelfisch <cankinik@web.de>
2025-01-26 19:22:08 +0100

    create tab bar item

---
+++ lib/widgets/homescreen/tab_bar_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class TabBarItem extends StatelessWidget {
+  const TabBarItem({super.key, required this.itemText});
+  final String itemText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Tab(
+      child: Container(
+        height: 40,
+        width: 80,
+        decoration: BoxDecoration(
+            color: Color.fromRGBO(255, 255, 255, 0.2),
+            borderRadius: BorderRadius.circular(50),
+            border: Border.all(
+                color: const Color.fromRGBO(255, 255, 255, 0.5), width: 1)),
+        child: Align(
+          alignment: Alignment.center,
+          child: Text(
+            itemText,
+            style: TextStyle(fontSize: 12),
+          ),
+        ),
+      ),
+    );
+  }
+}
